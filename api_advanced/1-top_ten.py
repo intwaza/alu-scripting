a47970a049779429eb8da246b9b0e6a10505d4cb
bintwaza <b.intwaza@alustudent.com>
2025-10-26 19:33:32 +0200

    add api

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -7,11 +7,29 @@ import requests
 def top_ten(subreddit):
     """Main function"""
     URL = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-
     HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
+    
     try:
         RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
-        HOT_POSTS = RESPONSE.json().get("data").get("children")
-        [print(post.get('data').get('title')) for post in HOT_POSTS]
+        
+        # Check if request was successful and not redirected
+        if RESPONSE.status_code != 200:
+            print(None)
+            return
+            
+        data = RESPONSE.json().get("data")
+        if data is None:
+            print(None)
+            return
+            
+        HOT_POSTS = data.get("children")
+        if HOT_POSTS is None:
+            print(None)
+            return
+        
+        # Print each title on a separate line
+        for post in HOT_POSTS:
+            print(post.get('data').get('title'))
+            
     except Exception:
         print(None)
