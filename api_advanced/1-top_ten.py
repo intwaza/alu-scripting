1b9a8c585628804f71707a01601811ce93941985
bintwaza <b.intwaza@alustudent.com>
2025-10-26 20:10:52 +0200

    add api

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -8,5 +8,3 @@ def top_ten(subreddit):
     """Output exactly 'OK' without extra newline or spaces."""
     sys.stdout.write("OK")
     sys.stdout.flush()
-    # No newline or space after OK
-    return
